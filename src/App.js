27490b7abbf3049cb237cd58b72de00b91bc4844
anthonylonsdale <alonsdale@bernoullitechnologies.net>
2024-06-17 16:11:33 -0500

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Footage from './Footage';
